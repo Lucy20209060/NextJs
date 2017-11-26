be6faf31fb7d5c23e0b2b08429c9a20f4f93df0c
luc <550502661@qq.com>
2017-11-26 23:46:52 +0800

    lu

--- .next/bundles/pages/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([5],{225:function(e,t,n){e.exports=n(226)},226:function(e,t,n){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=n(8),l=u(a),r=n(1),f=u(r),o=n(2),d=u(o),i=n(9),s=u(i),c=n(10),p=u(c),_=n(5),m=u(_),v=function(e){function t(){(0,f.default)(this,t);var e=(0,s.default)(this,(t.__proto__||(0,l.default)(t)).call(this));return e.state={siteInfo:{name:"index page"}},e}return(0,p.default)(t,e),(0,d.default)(t,[{key:"render",value:function(){return m.default.createElement("div",null,m.default.createElement("p",null,this.state.siteInfo.name))}}]),t}(m.default.Component);t.default=v}},[225]);
-            return { page: comp.default }
-          })
-        
